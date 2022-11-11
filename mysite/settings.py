47f7fdfdc4b343ecdc0eac4259e9ae385b337649
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2022-11-11 18:26:00 +0530

    deploy-setup and requirement optimization

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zb2-iiufq9op+qbf4dv@i4ae(e0xe9)jvl858buf5-^uugy1iv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
